cd51e3088433fd95be39ae29b1de32ecdcd7ef15
Tomas Patro <tomas.patro@gmail.com>
2019-09-26 22:42:47 -0600

    Added JavaDoc comments to MainActivity class

--- app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/MainActivity.java
@@ -9,11 +9,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * Represents the main activity of the application which represents the first and main screen shown
+ * to user after they run the application.
+ *
+ * @author Tomas Patro
+ */
 public class MainActivity extends AppCompatActivity {
     private TextView nameView, initialPriceView, currentPriceView, percentageChangeView;
     private Button updatePriceButton, openItemWebsiteButton;
     private PriceFinder priceFinder;
 
+    /**
+     * Method which is called when the activity is created. It is used to initialize graphic
+     * components of the application along to private attributes of the class. It is also used to
+     * initialize URL of the initial item which can be derived from the external source using the
+     * {@link Intent} class. Otherwise, default value is being used which is
+     * <a href="https://www.utep.edu/">UTEP's main website</a>.
+     *
+     * @param savedInstanceState an attribute representing the saved state of the current instance
+     *                           of the activity
+     * @see Intent
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
         String type = getIntent().getType();
         String url;
 
-        if (Intent.ACTION_SEND.equalsIgnoreCase(action) && type != null && "text/plain".equals(type))
+        if (Intent.ACTION_SEND.equalsIgnoreCase(action) && type != null &&
+                "text/plain".equals(type))
             url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
         else
             url = "https://www.utep.edu/";
@@ -44,6 +62,14 @@ public class MainActivity extends AppCompatActivity {
         percentageChangeView.setText("-");
     }
 
+    /**
+     * Handler for the click event on the {@code updatePriceButton} button. It sets the text of the
+     * {@code currentPriceView} to the value of current price of the initial item and text of the
+     * {@code initialPriceView} to the value of initial price of the initial item. It also updates
+     * the percentage change between the initial and current price.
+     *
+     * @param view current view where the {@code updatePriceButton} is located in
+     */
     public void updatePriceClicked(View view) {
         currentPriceView.setText(String.valueOf(priceFinder.getInitialItemCurrentPrice()));
         initialPriceView.setText(String.valueOf(priceFinder.getInitialItemInitialPrice()));
@@ -56,8 +82,15 @@ public class MainActivity extends AppCompatActivity {
             percentageChangeView.setText(String.format("%.2f", percentageChange) + "%");
     }
 
+    /**
+     * Handler for the click event on the {@code openItemWebsiteButton} button. It opens the browser
+     * and loads the website with the URL of the initial item.
+     *
+     * @param view current view where the {@code openItemWebsiteButton} is located in
+     */
     public void openItemWebsiteClicked(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(priceFinder.getInitialItemUrl()));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse(priceFinder.getInitialItemUrl()));
         startActivity(browserIntent);
     }
 }
