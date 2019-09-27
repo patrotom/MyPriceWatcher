36133ee698e2241fda67766d27effcfb54f0ef01
Tomas Patro <tomas.patro@gmail.com>
2019-09-26 22:59:37 -0600

    Added JavaDoc comments to Item class

--- app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/Item.java
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/Item.java
@@ -1,24 +1,51 @@
 package edu.utep.cs.cs4330.mypricewatcher;
 
+/**
+ * Represents the item which is the direct representation of the product with its price.
+ *
+ * @author Tomas Patro
+ */
 public class Item {
     private String url, name;
     private Long initialPrice, currentPrice;
     private boolean initialPriceSet;
 
+    /**
+     * Class constructor.
+     *
+     * @param name name of the item/product
+     * @param url URL of the item/product
+     */
     Item(String name, String url) {
         this.url = url;
         this.name = name;
         initialPriceSet = false;
     }
 
+    /**
+     * Standard getter method which returns the URL of the item/product.
+     *
+     * @return URL of the item/product
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Standard getter method which returns the name of the item/product.
+     *
+     * @return name of the item/product
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Standard getter method which returns the initial price of the item/product. If the initial
+     * price has not been set yet, returns 0.
+     *
+     * @return initial price of the item/product
+     */
     public Long getInitialPrice() {
         if (!initialPriceSet) {
             return new Long(0);
@@ -26,30 +53,67 @@ public class Item {
         return initialPrice;
     }
 
+    /**
+     * Standard getter method which returns the current price of the item/product.
+     *
+     * @return current price of the item/product
+     */
     public Long getCurrentPrice() {
         return currentPrice;
     }
 
+    /**
+     * Returns the {@code boolean} indicator whether the initial price has been already set or not.
+     *
+     * @return indicator whether the initial price has been already set
+     */
     public boolean getInitialPriceSet() {
         return initialPriceSet;
     }
 
+    /**
+     * Standard setter method which sets the value of the URL of the item/product.
+     *
+     * @param url new value of the URL to be set
+     */
     public void setUrl(String url) {
         this.url = url;
     }
 
+    /**
+     * Standard setter method which sets the value of the name of the item/product.
+     *
+     * @param name new value of the name to be set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Standard setter method which sets the value of the initial price of the item/product.
+     *
+     * @param initialPrice new value of the initial price to be set
+     */
     public void setInitialPrice(Long initialPrice) {
         this.initialPrice = initialPrice;
     }
 
+    /**
+     * Standard setter method which sets the value of the current price of the item/product.
+     *
+     * @param currentPrice new value of the current price to be set
+     */
     public void setCurrentPrice(Long currentPrice) {
         this.currentPrice = currentPrice;
     }
 
+    /**
+     * Standard setter method which sets the value of the indicator whether the initial price
+     * of the item/product has been already set or not.
+     *
+     * @param initialPriceSet new value of the indicator whether the initial price of the
+     *                        item/product has been already set
+     */
     public void setInitialPriceSet(boolean initialPriceSet) {
         this.initialPriceSet = initialPriceSet;
     }
