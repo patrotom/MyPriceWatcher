c1a68f3fe8900a704629b3d721d1210e546e2ecf
Tomas Patro <tomas.patro@gmail.com>
2019-09-26 23:05:24 -0600

    Added JavaDoc comments to PriceFindBehavior interface

--- app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/PriceFindBehavior.java
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/PriceFindBehavior.java
@@ -1,5 +1,20 @@
 package edu.utep.cs.cs4330.mypricewatcher;
 
+/**
+ * Interface which is representing the behavior of the finding the price of the product. It is used
+ * in the strategy pattern for defining different strategies of the finding the price of the
+ * product.
+ *
+ * @author Tomas Patro
+ */
 public interface PriceFindBehavior {
-    public Long findPrice(Item item);
+    /**
+     * Interface method for finding the price of the product. Each strategy implementing this
+     * interface has to implement this method.
+     *
+     * @param item represents the item for which we want to find the current price
+     * @return current price of the product
+     * @see Item
+     */
+    Long findPrice(Item item);
 }
