ba0bed2be9ee37c86b17e24031224d2ddfc3d745
Tomas Patro <tomas.patro@gmail.com>
2019-10-19 18:03:09 -0600

    Added list fragment

--- app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/MainActivity.java
@@ -1,13 +1,20 @@
 package edu.utep.cs.cs4330.mypricewatcher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Represents the main activity of the application which represents the first and main screen shown
@@ -19,8 +26,6 @@ import android.widget.TextView;
  * @see SimulatedBehavior
  */
 public class MainActivity extends AppCompatActivity {
-    private TextView nameView, initialPriceView, currentPriceView, percentageChangeView;
-    private Button updatePriceButton, openItemWebsiteButton;
     private PriceFinder priceFinder;
 
     /**
@@ -39,62 +44,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        nameView = findViewById(R.id.nameView);
-//        initialPriceView = findViewById(R.id.initialPriceView);
-//        currentPriceView = findViewById(R.id.currentPriceView);
-//        percentageChangeView = findViewById(R.id.percentageChangeView);
-//        updatePriceButton = findViewById(R.id.updatePriceButton);
-//        openItemWebsiteButton = findViewById(R.id.openItemWebsiteButton);
-//
-//        String action = getIntent().getAction();
-//        String type = getIntent().getType();
-//        String url;
-//
-//        if (Intent.ACTION_SEND.equalsIgnoreCase(action) && type != null &&
-//                "text/plain".equals(type))
-//            url = getIntent().getStringExtra(Intent.EXTRA_TEXT);
-//        else
-//            url = "https://www.utep.edu/";
-//
-//        priceFinder = new PriceFinder(new SimulatedBehavior(), new Item("Dummy Item", url));
-//
-//        nameView.setText(priceFinder.getInitialItemName());
-//        initialPriceView.setText(String.valueOf(priceFinder.getInitialItemInitialPrice()));
-//
-//        currentPriceView.setText("-");
-//        percentageChangeView.setText("-");
+        priceFinder = new PriceFinder(new SimulatedBehavior());
+
+        ItemListFragment fragment = (ItemListFragment)
+                getSupportFragmentManager().findFragmentById(R.id.itemListFragment);
+        fragment.setListener(this::itemClicked);
     }
 
-    /**
-     * Handler for the click event on the {@code updatePriceButton} button. It sets the text of the
-     * {@code currentPriceView} to the value of current price of the initial item and text of the
-     * {@code initialPriceView} to the value of initial price of the initial item. It also updates
-     * the percentage change between the initial and current price and set this value to the
-     * {@code percentageChangeView} view.
-     *
-     * @param view current view where the {@code updatePriceButton} is located in
-     */
-    public void updatePriceClicked(View view) {
-        currentPriceView.setText(String.valueOf(priceFinder.getInitialItemCurrentPrice()));
-        initialPriceView.setText(String.valueOf(priceFinder.getInitialItemInitialPrice()));
+    private void itemClicked(String name) {
 
-        Double percentageChange = priceFinder.countPercentageChange();
-
-        if (percentageChange > 0.0)
-            percentageChangeView.setText(String.format("+" + "%.2f", percentageChange) + "%");
-        else
-            percentageChangeView.setText(String.format("%.2f", percentageChange) + "%");
-    }
-
-    /**
-     * Handler for the click event on the {@code openItemWebsiteButton} button. It opens the browser
-     * and loads the website with the URL of the initial item.
-     *
-     * @param view current view where the {@code openItemWebsiteButton} is located in
-     */
-    public void openItemWebsiteClicked(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW,
-                Uri.parse(priceFinder.getInitialItemUrl()));
-        startActivity(browserIntent);
     }
 }
--- app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/PriceFinder.java
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/PriceFinder.java
@@ -1,5 +1,8 @@
 package edu.utep.cs.cs4330.mypricewatcher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represents the handler for the items/products. In the current version, this class only handles
  * the initial item. It uses different strategies from the strategy pattern to get the price of the
@@ -12,20 +15,17 @@ package edu.utep.cs.cs4330.mypricewatcher;
  * @see ScraperBehavior
  */
 public class PriceFinder {
-    private Item initialItem;
+    private static ArrayList<Item> items;
     private PriceFindBehavior strategy;
 
     /**
      * Class constructor.
      *
      * @param strategy strategy which is used to calculate the price
-     * @param initialItem initial item used to get its initial price, current price and percentage
-     *                    change
      */
-    PriceFinder(PriceFindBehavior strategy, Item initialItem) {
+    PriceFinder(PriceFindBehavior strategy) {
         this.strategy = strategy;
-        this.initialItem = initialItem;
-        this.initialItem.setCurrentPrice(this.strategy.findPrice(this.initialItem));
+        items = new ArrayList<>();
     }
 
     /**
@@ -33,56 +33,20 @@ public class PriceFinder {
      *
      * @return percentage change between the initial and current price of the initial item
      */
-    public Double countPercentageChange() {
-        return ((double)(initialItem.getCurrentPrice() - initialItem.getInitialPrice()) /
-                (double)initialItem.getInitialPrice()) * 100;
+    public Double percentageChange(Item item) {
+        return ((double)(item.getCurrentPrice() - item.getInitialPrice()) /
+                (double)item.getInitialPrice()) * 100;
     }
 
-    /**
-     * Standard getter method which returns the URL of the initial item.
-     *
-     * @return URL of the initial item
-     */
-    public String getInitialItemUrl() {
-        return initialItem.getUrl();
+    public void addItem(Item item) {
+        items.add(item);
     }
 
-    /**
-     * Standard getter method which returns the name of the initial item.
-     *
-     * @return name of the initial item
-     */
-    public String getInitialItemName() {
-        return initialItem.getName();
-    }
-
-    /**
-     * Standard getter method which returns the current price of the initial item. Before returning
-     * the current price, it checks whether there is a change in the current price and sets the
-     * new value.
-     *
-     * @return current price of the initial item
-     */
-    public Long getInitialItemCurrentPrice() {
-        initialItem.setCurrentPrice(strategy.findPrice(initialItem));
-        return initialItem.getCurrentPrice();
-    }
-
-    /**
-     * Standard getter method which returns the initial price of the initial item.
-     *
-     * @return initial price of the initial item
-     */
-    public Long getInitialItemInitialPrice() {
-        return initialItem.getInitialPrice();
-    }
-
-    /**
-     * Standard setter method which sets a new strategy for calculating the price of the item.
-     *
-     * @param strategy designated strategy to be used to calculate a price of the item
-     */
-    public void setStarategy(PriceFindBehavior strategy) {
-        this.strategy = strategy;
+    public static List<String> names() {
+        List<String> names = new ArrayList<>(items.size());
+        for (Item i: items) {
+            names.add(i.getName());
+        }
+        return names;
     }
 }
